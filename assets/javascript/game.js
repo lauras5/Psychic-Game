fe22faeb6450b7586e9720810b0510f30a533650
Laura Salcido <idlaurasalcido@gmail.com>
2018-02-05 18:27:30 -0800

    final game, backup in case I can't finish hangman

---
+++ assets/javascript/game.js
@@ -0,0 +1,52 @@
+
+$(document).ready(function() {
+    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    var lettersLength = letters.length
+    var letterToGuess = null;
+    var rLetter = (letters[Math.floor(Math.random() * lettersLength)])
+    //   console.log(rLetter)
+    var wins = 0;
+        $(".wins").html("Wins : " + wins)
+    var losses = 0;
+        $(".losses").html("Losses : " + losses)
+    var guessesLeft = 9
+        $(".guessesLeft").html("You have " + guessesLeft + " guesses left!")
+    var guesses = []
+        $(".guesses").html([])
+    var newLetter = function() {
+        this.letterToGuess = this.letters[Math.floor(Math.random() * this.lettersLength)]
+    }
+
+    var reset = function() {
+        guesses = $(".guesses").html([" "])
+        guessesLeft = 9;
+        rLetter = (letters[Math.floor(Math.random() * lettersLength)])
+    }
+
+    console.log(rLetter)
+    document.onkeyup = function(event) {
+        var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+        console.log(userGuess)
+        if(guessesLeft > 0){
+            if(userGuess != rLetter) {
+                // alert("wrong!")
+                $(".guesses").append(userGuess);
+                $(".guessesLeft").html("You have " + guessesLeft-- + " guesses left!")
+            }
+            if(userGuess === rLetter) {
+                alert("You Win! You're a Psychic! The letter was '" + rLetter + "'!")
+                $(".wins").html("Wins : " + wins++)
+                reset()
+            }
+
+
+        }
+
+        else if (guessesLeft === 0) {
+            alert("You Lose! The answer was '" + rLetter + "'. Try again!")
+            $(".losses").html("Losses : " + losses++)
+            reset()
+        }
+    }
+})
