aec80c1819600440ac43a97a66409d6c4bdf5a6f
Laura Salcido <idlaurasalcido@gmail.com>
2018-02-13 14:52:14 -0800

    cleaning code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,15 @@
-
 $(document).ready(function() {
     var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     var lettersLength = letters.length
-    var letterToGuess = null;
+    var letterToGuess = ''
+    // generates random letter
     var rLetter = (letters[Math.floor(Math.random() * lettersLength)])
-    //   console.log(rLetter)
+      console.log(rLetter)
     var wins = 0;
         $(".wins").html("Wins : " + wins)
     var losses = 0;
         $(".losses").html("Losses : " + losses)
-    var guessesLeft = 9
+    var guessesLeft = 10
         $(".guessesLeft").html("You have " + guessesLeft + " guesses left!")
     var guesses = []
         $(".guesses").html([])
@@ -17,35 +17,35 @@ $(document).ready(function() {
         this.letterToGuess = this.letters[Math.floor(Math.random() * this.lettersLength)]
     }
 
+
     var reset = function() {
         guesses = $(".guesses").html([" "])
         guessesLeft = 9;
         rLetter = (letters[Math.floor(Math.random() * lettersLength)])
     }
 
-    console.log(rLetter)
     document.onkeyup = function(event) {
         var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-
+        
         console.log(userGuess)
         if(guessesLeft > 0){
             if(userGuess != rLetter) {
                 // alert("wrong!")
                 $(".guesses").append(userGuess);
-                $(".guessesLeft").html("You have " + guessesLeft-- + " guesses left!")
+                guessesLeft--
+                $(".guessesLeft").html("You have " + guessesLeft + " guesses left!")
             }
             if(userGuess === rLetter) {
                 alert("You Win! You're a Psychic! The letter was '" + rLetter + "'!")
-                $(".wins").html("Wins : " + wins++)
-                reset()
+                wins++
+                $(".wins").html("Wins : " + wins)
             }
-
-
         }
-
+        
         else if (guessesLeft === 0) {
             alert("You Lose! The answer was '" + rLetter + "'. Try again!")
-            $(".losses").html("Losses : " + losses++)
+            losses++
+            $(".losses").html("Losses : " + losses)
             reset()
         }
     }
