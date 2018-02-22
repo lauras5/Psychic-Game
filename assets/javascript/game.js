5d2951ce23d4a4d9d6f901d7fd7ae320f1c91f7e
Laura Salcido <idlaurasalcido@gmail.com>
2018-02-22 10:50:51 -0800

    restricted keycode to letter char, no numbers, fixed bug, cleaned code for portfolio

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     var lettersLength = letters.length
     var letterToGuess = ''
+  
     // generates random letter
     var rLetter = (letters[Math.floor(Math.random() * lettersLength)])
       console.log(rLetter)
@@ -26,7 +27,11 @@ $(document).ready(function() {
 
     document.onkeyup = function(event) {
         var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-        
+        var valid = (event.keyCode >= 65 && event.keyCode <= 90) || (event.keyCode >= 97 && event.keyCode <= 122);
+        if (!valid) {
+            e.preventDefault();
+        }
+
         console.log(userGuess)
         if(guessesLeft > 0){
             if(userGuess != rLetter) {
@@ -39,6 +44,7 @@ $(document).ready(function() {
                 alert("You Win! You're a Psychic! The letter was '" + rLetter + "'!")
                 wins++
                 $(".wins").html("Wins : " + wins)
+                reset()
             }
         }
         
